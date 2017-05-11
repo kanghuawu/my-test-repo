75887e910817e8d22d269f95385ed12fa78cb219
kanghuawu <kang-hua.wu@sjsu.edu>
2017-05-10 18:52:10 -0700

    change for demo

--- app.py
+++ app.py
@@ -5,34 +5,34 @@ from camera import Camera
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Flask App!!"
+# @app.route("/")
+# def hello():
+#     return "Hello from Flask App!!"
 
 '''
 reference: https://github.com/miguelgrinberg/flask-video-streaming/blob/master/app.py
 '''
 
 
-# @app.route('/')
-# def index():
-#     """Video streaming home page."""
-#     return render_template('index.html')
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
 
 
-# def gen(camera):
-#     """Video streaming generator function."""
-#     while True:
-#         frame = camera.get_frame()
-#         yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-# @app.route('/video_feed')
-# def video_feed():
-#     """Video streaming route. Put this in the src attribute of an img tag."""
-#     return Response(gen(Camera()),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
